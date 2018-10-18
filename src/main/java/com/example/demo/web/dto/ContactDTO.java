9dd45bda2af213d3d41c094112dbaa871f580e27
Irina <i.tyapysheva@gmail.com>
2018-10-18 22:59:34 +0400

    +Add ContactDTO

---
+++ src/main/java/com/example/demo/web/dto/ContactDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.web.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ContactDTO {
+    private Integer id;
+    private String name;
+}
